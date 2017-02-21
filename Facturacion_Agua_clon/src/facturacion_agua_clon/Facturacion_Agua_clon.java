8362ae6551bc46699275eafd421883032bff040f
javit <javit@JAVIER-PORTATIL>
2017-02-21 16:54:22 +0000

    Prueba de error

--- Facturacion_Agua_clon/src/facturacion_agua_clon/Facturacion_Agua_clon.java
+++ Facturacion_Agua_clon/src/facturacion_agua_clon/Facturacion_Agua_clon.java
@@ -15,6 +15,7 @@ public class Facturacion_Agua_clon {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        LOG.info("Inicio de la aplicación: " + args.length + " argumentos");
         int prueba;
         int otra;
     }
